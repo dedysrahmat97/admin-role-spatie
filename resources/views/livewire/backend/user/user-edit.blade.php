1be0c787e1595f21f7d55a6d38e09b9131bd2ab3
dedysrahmat97 <dreesrahmat@gmail.com>
2025-05-30 08:21:54 +0700

    style: increase dropdown height and improve no results message in user edit component

--- resources/views/livewire/backend/user/user-edit.blade.php
+++ resources/views/livewire/backend/user/user-edit.blade.php
@@ -78,13 +78,17 @@
                                         return `<input class='input input-bordered w-full' autocomplete='off' />`;
                                     },
                                     option: function(item, escape) {
-                                        return `<div class='flex items-center p-2 hover:bg-primary hover:text-primary-content'>                                                                            <span class='badge badge-primary mr-2'>${escape(item.name)}</span>
+                                        return `<div class='flex items-center p-2 hover:bg-primary hover:text-primary-content'>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                <span class='badge'>${escape(item.name)}</span>
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             </div>`;
                                     },
                                     item: function(item, escape) {
                                         return `<div class='badge badge-primary'>
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 <span>${escape(item.name)}</span>
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             </div>`;
+                                    },
+                                    no_results: function() {
+                                        return `<div class='py-2 flex items-center justify-center h-16 text-error text-center bg-base-200 rounded'>Tidak ada hasil ditemukan</div>`;
                                     }
                                 }
                             });" @set-reset.window="$el.roles.clear()" id="form.roles"
